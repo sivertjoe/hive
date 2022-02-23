bf670ba7dc629c8ad78ad2c7674db4d9d94a8605
sivertjoe <sivert-joh@hotmail.com>
2022-02-23 21:46:57 +0100

    improve navbar

--- frontend/src/component/navbar.rs
+++ frontend/src/component/navbar.rs
@@ -2,7 +2,29 @@ use seed::{prelude::*, *};
 
 use crate::{Model, Msg};
 
-pub fn view(model: &Model) -> Node<Msg>
+
+fn capitalize(s: &str) -> String
 {
-    div![C!("navbar"), div![C!("menu"), div![C!("items"), h2!["register",]]]]
+    let mut c = s.chars();
+    match c.next()
+    {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+    }
+}
+
+fn create_link(text: &str) -> Node<Msg>
+{
+    let _text = capitalize(text);
+
+    h2![a![_text, attrs! { At::Href => text }]]
+}
+
+pub fn view(_model: &Model) -> Node<Msg>
+{
+    div![C!("navbar"), div![C!("menu"), div![
+        C!("items"),
+        create_link("register"),
+        create_link("login")
+    ]]]
 }
