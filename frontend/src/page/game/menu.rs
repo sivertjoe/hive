39435580ddae64335de4f5efd244ef18419345f5
sivertjoe <sivert-joh@hotmail.com>
2022-03-27 22:35:07 +0200

    add menu with numbers

--- frontend/src/page/game/menu.rs
+++ frontend/src/page/game/menu.rs
@@ -3,16 +3,74 @@ use super::Msg;
 use seed::{self, prelude::*, *};
 use shared::model::*;
 
-pub struct MenuItem {
-    x: f32,
-    y: f32,
+pub struct Menu {
+    items: Vec<MenuEntry>,
+}
+
+impl Menu {
+    pub fn new<I: Iterator<Item = Piece>>(pieces: I, board: &Board) -> Self {
+        let items = pieces
+            .map(|piece| MenuEntry {
+                count_left: piece_to_count(piece.r#type),
+                piece,
+            })
+            .collect();
+
+        let mut menu = Menu { items };
+        let color = menu.items.first().unwrap().piece.color;
+
+        for square in board.board.values() {
+            for bp in square
+                .pieces
+                .iter()
+                .filter_map(|piece| (piece.color == color).then(|| piece.r#type))
+            {
+                menu.reduce_piece(bp);
+            }
+        }
+
+
+        menu
+    }
+    pub fn to_node(&self) -> Node<crate::Msg> {
+        div![
+            style! {
+                St::Display => "flex",
+            },
+            self.items
+                .iter()
+                .filter_map(|entry| (entry.count_left > 0).then(|| entry.to_node()))
+        ]
+    }
+
+    pub fn reduce_piece(&mut self, bp: BoardPiece) {
+        self.items
+            .iter_mut()
+            .find(|entry| entry.piece.r#type == bp)
+            .unwrap()
+            .count_left -= 1;
+    }
+}
+
+#[inline]
+fn piece_to_count(bp: BoardPiece) -> usize {
+    match bp {
+        BoardPiece::Ant => 3,
+        BoardPiece::Grasshopper => 3,
+        BoardPiece::Spider => 2,
+        BoardPiece::Beetle => 2,
+        BoardPiece::Queen => 1,
+    }
+}
+
+// Maybe rename stuff later
+pub struct MenuEntry {
+    count_left: usize,
     piece: Piece,
 }
 
-impl MenuItem {
-    pub fn to_menu_node(&self) -> Node<crate::Msg> {
-        let (x, y) = (self.x, self.y);
-
+impl MenuEntry {
+    pub fn to_node(&self) -> Node<crate::Msg> {
         let stroke = piece_color(self.piece.r#type, self.piece.color);
         let id = format!("{:?}", self.piece.r#type);
 
@@ -31,7 +89,6 @@ impl MenuItem {
 
                 let el: &Element = idv.as_ref();
                 let id = el.id();
-                // This is a big yikes; however, good enough for now.
 
                 ev.data_transfer()
                     .unwrap()
@@ -46,40 +103,12 @@ impl MenuItem {
                     St::Width => "90px",//self.spacing,
                     St::Height => "90px", // ??
                     St::Background => stroke,
+                    St::Color => "black",
             },
             attrs! {
-                    At::X => x,
-                    At::Y => y,
                     At::Draggable => "true",
-            }
+            },
+            h1!(self.count_left)
         ]
     }
-}
-
-pub fn create_menu(color: Color) -> Vec<MenuItem> {
-    let deltas = |n: f32| (15. * n, 9. * n);
-    let (_, dy) = deltas(0.5);
-
-    use BoardPiece::*;
-    let colors = [Queen, Ant, Spider, Grasshopper, Beetle];
-    let _spacing = 100.0 / colors.len() as f32;
-
-
-    // Should be set to the players colors
-    let len = colors.len();
-
-    colors
-        .into_iter()
-        .enumerate()
-        .map(|(i, bp)| {
-            let piece = Piece { r#type: bp, color };
-            let size = 100.0 / len as f32;
-            let per = size / 2.0; // center
-            let x = ((i + 1) as f32 * size) - per;
-
-            let y = 1.65 * dy; // ???
-
-            MenuItem { x, y, piece }
-        })
-        .collect()
 }
